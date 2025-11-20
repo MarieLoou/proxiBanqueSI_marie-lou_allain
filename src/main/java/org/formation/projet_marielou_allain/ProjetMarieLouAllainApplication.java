218d698db2e29433634aa4e6ba68e1457373721d
Marie-Lou Thinkpad <marie-lou.allain@epita.fr>
2025-11-20 15:38:05 +0100

    add repositories

--- src/main/java/org/formation/projet_marielou_allain/ProjetMarieLouAllainApplication.java
+++ src/main/java/org/formation/projet_marielou_allain/ProjetMarieLouAllainApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProjetMarieLouAllainApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProjetMarieLouAllainApplication.class, args);
+
+
     }
 
 }
---
+++ src/main/java/org/formation/projet_marielou_allain/repository/AgencyRepository.java
@@ -0,0 +1,7 @@
+package org.formation.projet_marielou_allain.repository;
+
+import org.formation.projet_marielou_allain.entity.Agency;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AgencyRepository extends JpaRepository<Agency, Long> {
+}
